5df209e697a38036bc7c3f79e5afea1f30b9003e
Users666 <basmanwahyudi40@gmail.com>
2026-04-08 02:52:51 +0700

    Fix koneksi env

--- db.js
+++ db.js
@@ -1,20 +1,12 @@
+require('dotenv').config(); // <--- TAMBAHKAN INI DI BARIS 1
 const mysql = require('mysql2');
-require('dotenv').config(); // Supaya bisa baca file .env atau Variables Railway
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,     // Mengambil data dari variabel DB_HOST di Railway
-  user: process.env.DB_USER,     // Mengambil data dari variabel DB_USER di Railway
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('Gagal koneksi ke database Railway:', err.message);
-    return;
-  }
-  console.log('Mantap! Berhasil konek ke Database Railway.');
 });
 
 module.exports = db;
