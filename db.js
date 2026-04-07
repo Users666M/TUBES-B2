95ea4cba6839611861a07b4dbf4746ccfada5ae0
Users666 <basmanwahyudi40@gmail.com>
2026-04-08 02:46:37 +0700

    Update db.js untuk koneksi Railway

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Supaya bisa baca file .env atau Variables Railway
 
 const db = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: '',
-database: 'db_express'
+  host: process.env.DB_HOST,     // Mengambil data dari variabel DB_HOST di Railway
+  user: process.env.DB_USER,     // Mengambil data dari variabel DB_USER di Railway
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Gagal koneksi ke database Railway:', err.message);
+    return;
+  }
+  console.log('Mantap! Berhasil konek ke Database Railway.');
 });
 
 module.exports = db;
